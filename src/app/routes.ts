e4610b291accb81ab89a4480552629208c772a07
sarah <sara.hoeferer@stud.fh-campuswien.ac.at>
2023-06-03 11:59:31 +0200

    created a seperated html file

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 
 const routeConfig: Routes = [
     {
       path: '',
-      component: HomeComponent,
+      component: MainComponent,
       title: 'Home page'
     },
   ];
